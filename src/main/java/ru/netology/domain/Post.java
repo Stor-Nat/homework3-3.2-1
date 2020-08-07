f0d2733cdb754463ee7e44e30c2db8f7906eb6bb
Natalya Storchilova <nata@Storcilovas-MacBook-Pro.local>
2020-08-07 19:46:35 +0300

    Added classes

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -7,13 +7,19 @@ public class Post {
     private String fromId;    // id кто оставил запись
     private int date;         // дата опубликования
     private String text;      // текст статьи
+
+    private int replyOwnerId; // идентификатор владельца записи, в ответ на которую была оставлена текущая
+    private int replyPostId;  // идентификатор записи, в ответ на которую была оставлена текущая
+
     private String copyright; // источник статьи
-    private int countLike;    // счетчик лайков
+
     private int countComments; // счетчик комментариев
     private int countRepost;  // счетчик репостов
     private int countView;    // кол-во просмотров
 
-    private CommentsInfo commentsInfo;  //
+    private CountLike countLike;  // информация о лайках к записи, объект с полями
+    private Geo geo;              // информация о местоположении , содержит поля
+    private CommentsInfo commentsInfo;  // информация о комментариях к записи, объект с полями
 
     // + get/set на все поля
 }
